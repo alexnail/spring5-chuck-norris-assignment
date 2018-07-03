7c32f271174b40befd506414bad999ce718ea63c
Aleksandrs Fridmans <alexnail@gmail.com>
2018-07-03 22:50:56 +0300

    Initial commit

---
+++ src/main/java/guru/springframework/spring5chucknorrisassignment/Spring5ChuckNorrisAssignmentApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5chucknorrisassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ChuckNorrisAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5ChuckNorrisAssignmentApplication.class, args);
+    }
+}
