ad89f8cde4006498aefff95e91901bbff4658d4e
Aleksandrs Fridmans <alexnail@gmail.com>
2018-07-04 21:24:16 +0300

    Part 3 of assignment

---
+++ src/main/java/guru/springframework/spring5chucknorrisassignment/services/JokeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5chucknorrisassignment.services;
+
+/**
+ * @author <a href="mailto:alexnail@gmail.com">Alexander Fridman</a>
+ * Date: 7/4/18
+ * Time: 9:05 PM
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/guru/springframework/spring5chucknorrisassignment/services/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring5chucknorrisassignment.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author <a href="mailto:alexnail@gmail.com">Alexander Fridman</a>
+ * Date: 7/4/18
+ * Time: 9:06 PM
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
